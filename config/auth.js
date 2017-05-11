1c516445fa3644473311be972f42812a8c3338fe
Teaumate <se56@laposte.net>
2017-05-11 14:39:17 +0200

    auth

--- config/auth.js
+++ config/auth.js
@@ -4,20 +4,20 @@
 module.exports = {
 
 	'facebookAuth' : {
-		'clientID' 		: '1773399452989042', // your App ID
-		'clientSecret' 	: '73558696b4802b6597a2acf77ee2fe04', // your App Secret
+		'clientID' 		: '', // your App ID
+		'clientSecret' 	: '', // your App Secret
 		'callbackURL' 	: 'http://127.0.0.1:8080/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
-		'consumerKey' 		: 'FFrQbx6YJ3i1y7SBF8ez2qshV',
-		'consumerSecret' 	: 'Hhi7zn7YkKGrSouzzXjyEWWqYuPTMUCR8mEvhbubVO1lDWBWLd',
+		'consumerKey' 		: '',
+		'consumerSecret' 	: '',
 		'callbackURL' 		: 'http://127.0.0.1:8080/auth/twitter/callback'
 	},
 
 	'googleAuth' : {
-		'clientID' 		: '125795165533-aorjjvpb02r32ns1j426ss979gihiobm.apps.googleusercontent.com',
-		'clientSecret' 	: 'bS9L1BcOzulgNk-1b672qPgV',
+		'clientID' 		: '.apps.googleusercontent.com',
+		'clientSecret' 	: '',
 		'callbackURL' 	: 'http://127.0.0.1:8080/auth/google/callback'
 	}
 
