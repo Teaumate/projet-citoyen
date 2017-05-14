814732e22f56f7d3b4bb9b4a5f168bc188484a1d
Teaumate <se56@laposte.net>
2017-05-15 01:15:49 +0200

    heroku

---
+++ config/auth.js
@@ -0,0 +1,24 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: '256637078077085', // your App ID
+		'clientSecret' 	: '54b6d6e25e0fb2d10e683bbae2fa761c', // your App Secret
+		'callbackURL' 	: 'http://127.0.0.1:8080/auth/facebook/callback'
+	},
+
+	'twitterAuth' : {
+		'consumerKey' 		: 'FFrQbx6YJ3i1y7SBF8ez2qshV',
+		'consumerSecret' 	: 'Hhi7zn7YkKGrSouzzXjyEWWqYuPTMUCR8mEvhbubVO1lDWBWLd',
+		'callbackURL' 		: 'http://127.0.0.1:8080/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID' 		: '125795165533-aorjjvpb02r32ns1j426ss979gihiobm.apps.googleusercontent.com',
+		'clientSecret' 	: 'bS9L1BcOzulgNk-1b672qPgV',
+		'callbackURL' 	: 'http://127.0.0.1:8080/auth/google/callback'
+	}
+
+};
